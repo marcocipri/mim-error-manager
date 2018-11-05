34b9ef11ea474181b3c9c955351f07dc6d92d15c
Marco Cipri <mcipri@IT01MN00002.local>
2018-11-05 11:28:32 +0100

    refactor

--- cooking_protocol/cooking_protocol/cooking_protocol.py
+++ cooking_protocol/cooking_protocol/cooking_protocol.py
@@ -1,3 +1,29 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
+
+from abc import ABC, abstractmethod
+
+class CookingProtocol(ABC):
+    mex_type = None
+    message = None
+
+
+class coocking_protocol_message_factory(ABC):
+    # Create based on class name:
+    def factory(rawInMex):
+        pass
+
+    def convertInMex(rawInMex, mexType):
+        pass
+
+    def convertOutMex(rawInMex, mexType):
+        pass
+
+    def defineMex(rawInMex):
+        pass
+
+    factory = staticmethod(factory)
+    defineMex = staticmethod(defineMex)
+    convertInMex = staticmethod(convertInMex)
+    convertOutMex = staticmethod(convertOutMex)
